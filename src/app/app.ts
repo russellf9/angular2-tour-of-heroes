2869cd171b7ab17a512c09b7bff70614bb31ed96
Russell Wenban <russell@factornine.co.uk>
2015-12-03 21:57:44 +0000

    Initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,17 @@
+import {bootstrap, Component} from 'angular2/angular2';
+@Component({
+    selector: 'my-app',
+    template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>'
+})
+class AppComponent {
+    public title = 'Tour of Heroes';
+    public hero:Hero = {
+        id: 1,
+        name: 'Windstorm'
+    }
+}
+class Hero {
+    id:number;
+    name:string;
+}
+bootstrap(AppComponent);
