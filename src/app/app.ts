6ccee301f65dbd508ee1f8ce4956033928f1bd33
Russell Wenban <russell@factornine.co.uk>
2015-12-05 16:21:45 +0000

    Add list of Heroes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import {bootstrap, Component, FORM_DIRECTIVES} from 'angular2/angular2';
+import {bootstrap, Component, FORM_DIRECTIVES, CORE_DIRECTIVES} from 'angular2/angular2';
 
 class Hero {
     id:number;
@@ -8,22 +8,61 @@ class Hero {
 
 @Component({
     selector: 'my-app',
-    template: `<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>
-               <div><label>id: </label>{{hero.id}}</div>
-               <div>
-                <label>name:</label>
-                <div><input [(ng-model)]="hero.name" placeholder="name"></div>
-               </div>
-               `,
-    directives: [FORM_DIRECTIVES]
+    template: `<h1>{{title}}</h1>
+               <h2>My Heroes</h2>
+               <ul class='heroes'>
+                  <li *ng-for='#hero of heroes'
+                      [ng-class]="getSelectedClass(hero)"
+                      (click)='onSelect(hero)'>
+                      <span class='badge'>{{hero.id}}</span> {{hero.name}}
+                  </li>
+               </ul>
+              <div *ng-if="selectedHero">
+                <h2>{{selectedHero.name}} details!</h2>
+                <div><label>id: </label>{{selectedHero.id}}</div>
+                <div><label>name:</label>
+                <div><input [(ng-model)]='selectedHero.name' placeholder='name'></div>                             </div>`,
+    styles: [`
+            .heroes {list-style-type: none; margin-left: 1em; padding: 0; width: 10em;}
+            .heroes li { cursor: pointer; position: relative; left: 0; transition: all 0.2s ease; }
+            .heroes li:hover {color: #369; background-color: #EEE; left: 0.2em;}
+            .heroes .badge {
+                font-size: small;
+                color: white;
+                padding: 0.1em 0.7em;
+                background-color: #369;
+                line-height: 1em;
+                position: relative;
+                left: -1px;
+                top: -1px;
+                }
+            .selected { background-color: #EEE; color: #369; }
+            `],
+    directives: [FORM_DIRECTIVES, CORE_DIRECTIVES]
 
 })
 class AppComponent {
     public title = 'Tour of Heroes';
-    public hero:Hero = {
-        id: 1,
-        name: 'Windstorm'
+    public selectedHero: Hero;
+    public heroes = HEROES;
+    onSelect(hero: Hero) { this.selectedHero = hero; };
+    getSelectedClass(hero: Hero) {
+        return { 'selected': hero === this.selectedHero };
     }
 }
 
+var HEROES:Hero[] = [
+    {'id': 11, 'name': 'Mr. Nice'},
+    {'id': 12, 'name': 'Narco'},
+    {'id': 13, 'name': 'Bombasto'},
+    {'id': 14, 'name': 'Celeritas'},
+    {'id': 15, 'name': 'Magneta'},
+    {'id': 16, 'name': 'RubberMan'},
+    {'id': 17, 'name': 'Dynama'},
+    {'id': 18, 'name': 'Dr IQ'},
+    {'id': 19, 'name': 'Magma'},
+    {'id': 20, 'name': 'Tornado'}
+];
+
 bootstrap(AppComponent);
+
