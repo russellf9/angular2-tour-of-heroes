96715241cfebd3aea2ae807861857f2726665667
Russell Wenban <russell@factornine.co.uk>
2015-12-05 15:17:50 +0000

    Add `ng-model`

--- src/app/app.js
+++ src/app/app.js
@@ -8,6 +8,11 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var angular2_1 = require('angular2/angular2');
+var Hero = (function () {
+    function Hero() {
+    }
+    return Hero;
+})();
 var AppComponent = (function () {
     function AppComponent() {
         this.title = 'Tour of Heroes';
@@ -19,16 +24,12 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         angular2_1.Component({
             selector: 'my-app',
-            template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>'
+            template: "<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>\n               <div><label>id: </label>{{hero.id}}</div>\n               <div>\n                <label>name:</label>\n                <div><input [(ng-model)]=\"hero.name\" placeholder=\"name\"></div>\n               </div>\n               ",
+            directives: [angular2_1.FORM_DIRECTIVES]
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
     return AppComponent;
-})();
-var Hero = (function () {
-    function Hero() {
-    }
-    return Hero;
 })();
 angular2_1.bootstrap(AppComponent);
 //# sourceMappingURL=app.js.map
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,22 @@
-import {bootstrap, Component} from 'angular2/angular2';
+import {bootstrap, Component, FORM_DIRECTIVES} from 'angular2/angular2';
+
+class Hero {
+    id:number;
+    name:string;
+}
+
+
 @Component({
     selector: 'my-app',
-    template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>'
+    template: `<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>
+               <div><label>id: </label>{{hero.id}}</div>
+               <div>
+                <label>name:</label>
+                <div><input [(ng-model)]="hero.name" placeholder="name"></div>
+               </div>
+               `,
+    directives: [FORM_DIRECTIVES]
+
 })
 class AppComponent {
     public title = 'Tour of Heroes';
@@ -10,8 +25,5 @@ class AppComponent {
         name: 'Windstorm'
     }
 }
-class Hero {
-    id:number;
-    name:string;
-}
+
 bootstrap(AppComponent);
